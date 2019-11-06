589efac80fdf3251df5302db83afd56370d5afae
borgwang <badbobobo@gmail.com>
2019-11-06 18:50:16 +0800

    update

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 # .inputrc
+echo "sync inputrc..."
 cp .inputrc $HOME/.inputrc
 source $HOME/.inputrc
 
 # Vim
 # install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -f ~/.vim/autoload/plug.vim ]; then
+    echo "vim-plug installed"
+else
+    echo "vim-plug not installed"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # .vimrc
+echo "sync vimrc..."
 cp .vimrc $HOME/.vimrc
 cp -r .vim/snippet $HOME/.vim/
 vim +PlugInstall +qall 
 
 # Tmux
+echo "sync tmux config..."
 cp .tmux.conf $HOME/.tmux.conf
 tmux source-file $HOME/.tmux.conf
+
+echo "done"
 
